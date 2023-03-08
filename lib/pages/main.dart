5d70fffdd6e30152504cf726b9514776aa1bed4a
Apurba Khanra <apurbakhanra09@gmail.com>
2023-03-08 09:31:40 +0530

    day 2

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var name = 'Apurba Khanra';
+  
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome ${name} to Flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
